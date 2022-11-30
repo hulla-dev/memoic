23c4ec7b5709277a3acef8d063d5d5381b8246c6
samuelhulla <oficialny.hulla@gmail.com>
2022-11-30 14:00:03 +0100

    :recycle: Cleanup: Unused old package core

--- packages/memoic/createMemoic.ts
+++
@@ -1,9 +0,0 @@
-import type { Queries, QueryMap } from './types'
-import { get, prefetch } from './fetching'
-
-export function createMemoic<MemoicQueries extends QueryMap<Queries>>(queries: MemoicQueries) {
-  return {
-    get: get(queries),
-    prefetch: prefetch(queries),
-  }
-}
--- packages/memoic/index.ts
+++
@@ -1,3 +0,0 @@
-export { Memoic } from './Memoic'
-export { query } from './queries'
-export { createMemoic } from './createMemoic'
