fc2b32dab30c4e39ab6534d914be7258f652efb0
samuelhulla <oficialny.hulla@gmail.com>
2022-11-30 14:04:36 +0100

    :zap: Feat: query

---
+++ packages/core/queries.ts
@@ -0,0 +1,14 @@
+import type { UseQueryOptions } from '@tanstack/react-query'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function query<Fn extends (...args: any[]) => any, Result = ReturnType<Fn>>(
+  fn: Fn,
+  initialParams?: Parameters<Fn>,
+  options?: UseQueryOptions,
+) {
+  return {
+    queryFn: (...deps: Parameters<Fn>) => fn(...deps) as Result,
+    options,
+    initialParams,
+  }
+}
