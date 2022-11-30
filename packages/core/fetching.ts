3c5bd270f711156075151133c7fb89800a28f81a
samuelhulla <oficialny.hulla@gmail.com>
2022-11-30 14:07:26 +0100

    :zap: Feat: get + prefetch

---
+++ packages/core/fetching.ts
@@ -0,0 +1,46 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { Queries, QueryMap, Final, Plugin } from './types'
+
+export const get =
+  <MemoicQueries extends QueryMap<Queries>, MemoicPlugin extends Plugin>(
+    queries: MemoicQueries,
+    plugin?: MemoicPlugin,
+  ) =>
+  <K extends keyof MemoicQueries>(
+    key: K,
+    deps: Parameters<MemoicQueries[K]['queryFn']>,
+    params?: MemoicPlugin['queryOptions'],
+  ) => {
+    if (plugin && plugin.get) {
+      return plugin.get({
+        queryKey: [key, ...deps],
+        queryFn: queries[key].queryFn,
+        params,
+      })
+    }
+    return useQuery<Final<MemoicQueries[K]['queryFn']>>({
+      queryKey: [key, ...deps],
+      queryFn: queries[key].queryFn,
+    })
+  }
+
+export const prefetch =
+  <MemoicQueries extends QueryMap<Queries>, MemoicPlugin extends Plugin>(
+    queries: MemoicQueries,
+    plugin?: MemoicPlugin,
+  ) =>
+  async <K extends keyof MemoicQueries>(
+    key: K,
+    deps: Parameters<MemoicQueries[K]['queryFn']>,
+    params?: MemoicPlugin['preFetchOptions'],
+  ) => {
+    if (plugin && plugin.prefetch) {
+      return plugin.prefetch({
+        queryKey: [key, ...deps],
+        queryFn: queries[key].queryFn,
+        params,
+      })
+    }
+    const client = useQueryClient()
+    return client.prefetchQuery([key, ...deps], queries[key].queryFn)
+  }
