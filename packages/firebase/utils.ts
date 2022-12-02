580d8d0de13930cff05b0b00f1b61aae8a5723c4
samuelhulla <oficialny.hulla@gmail.com>
2022-12-02 17:03:49 +0100

    :zap: Feat: getSnapshot

--- packages/firebase/utils.ts
+++ packages/firebase/utils.ts
@@ -11,3 +11,21 @@ export function isLegacyRef<T extends DocumentData>(
 export function getRef<Fn extends (...args: any[]) => any>(queryFn: Fn, queryKey: QueryKey) {
   return queryFn(...(queryKey.slice(1) || [])) as NativeDocRef<Value<Fn>> | DocRef<Value<Fn>>
 }
+
+export async function getSnapshot<
+  Val extends DocumentData,
+  Ref extends DocRef<Val> | NativeDocRef<Val>,
+>(ref: Ref) {
+  if (isLegacyRef(ref)) {
+    return ref.get()
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  if (!ref.onSnapshot) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const { getDoc } = require('firebase/firestore') // eslint-disable-line @typescript-eslint/no-var-requires
+    return getDoc(ref)
+  }
+}
