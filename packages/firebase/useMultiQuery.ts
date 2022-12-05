3ac83a0ee524822e33277310dac28fe61e79cce1
samuelhulla <oficialny.hulla@gmail.com>
2022-12-05 18:55:02 +0100

    :zap: Feat: useMultiQuery

---
+++ packages/firebase/useMultiQuery.ts
@@ -0,0 +1,27 @@
+import type { QueryKey, UseQueryOptions } from '@tanstack/react-query'
+import { getRef } from './utils'
+import { useObserver } from './useObserver'
+import { useQuerySubscription } from './useQuerySubscription'
+import { GetVariation, ObserverType, Return } from './types'
+
+export function useMultiQuery<FetchFn extends GetVariation>({
+  queryKey,
+  queryFn,
+  options,
+}: {
+  queryKey: QueryKey
+  queryFn: FetchFn
+  options?: UseQueryOptions<Return<FetchFn, 'query'>, Error>
+}) {
+  const type: ObserverType = 'query'
+  const ref = getRef<FetchFn, typeof type>(queryFn, queryKey)
+  const subscribe = useQuerySubscription(ref)
+
+  return useObserver({
+    queryKey,
+    queryFn,
+    type,
+    subscribe,
+    options,
+  })
+}
