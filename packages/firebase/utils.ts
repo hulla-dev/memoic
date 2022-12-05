af20788e22a8e50f4e8ab13e0a784989512fa43a
samuelhulla <oficialny.hulla@gmail.com>
2022-12-05 12:26:32 +0100

    :zap: Feat: isDocument and support for queries and collections

--- packages/firebase/utils.ts
+++ packages/firebase/utils.ts
@@ -1,6 +1,31 @@
 import type { QueryKey } from '@tanstack/react-query'
-import type { DocumentData } from 'firebase/firestore'
-import type { DocRef, NativeDocRef, Value } from './types'
+import type { DocumentData, Query } from 'firebase/firestore'
+import type {
+  CollectionRef,
+  DocRef,
+  NativeCollection,
+  NativeDocRef,
+  NativeQuery,
+  ObserverType,
+  Value,
+} from './types'
+
+type QueryVariation<T extends DocumentData> = CollectionRef<T> | Query<T>
+
+type NativeQueryVariation<T extends DocumentData> = NativeCollection<T> | NativeQuery<T>
+
+export function isDocument(
+  ref:
+    | DocRef<DocumentData>
+    | NativeDocRef<DocumentData>
+    | QueryVariation<DocumentData>
+    | NativeQueryVariation<DocumentData>,
+): ref is DocRef<DocumentData> | NativeDocRef<DocumentData> {
+  return (
+    (ref as DocRef<DocumentData>)?.type === 'document' ||
+    (ref as NativeQueryVariation<DocumentData>)?.where === undefined
+  )
+}
 
 export function isLegacyRef<T extends DocumentData>(
   ref: NativeDocRef<T> | DocRef<T>,
@@ -8,24 +33,18 @@ export function isLegacyRef<T extends DocumentData>(
   return !!(ref as NativeDocRef<T>).onSnapshot
 }
 
-export function getRef<Fn extends (...args: any[]) => any>(queryFn: Fn, queryKey: QueryKey) {
-  return queryFn(...(queryKey.slice(1) || [])) as NativeDocRef<Value<Fn>> | DocRef<Value<Fn>>
+export function isLegacyQuery<T extends DocumentData>(
+  ref: QueryVariation<T> | NativeQueryVariation<T>,
+): ref is NativeQueryVariation<T> {
+  return !!(ref as NativeQueryVariation<T>).onSnapshot
 }
 
-export async function getSnapshot<
-  Val extends DocumentData,
-  Ref extends DocRef<Val> | NativeDocRef<Val>,
->(ref: Ref) {
-  if (isLegacyRef(ref)) {
-    return ref.get()
-  }
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  if (!ref.onSnapshot) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const { getDoc } = require('firebase/firestore') // eslint-disable-line @typescript-eslint/no-var-requires
-    return getDoc(ref)
-  }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getRef<Fn extends (...args: any[]) => any, O extends ObserverType>(
+  queryFn: Fn,
+  queryKey: QueryKey,
+) {
+  return queryFn(...(queryKey.slice(1) || [])) as O extends 'doc'
+    ? DocRef<Value<Fn>> | NativeDocRef<Value<Fn>>
+    : QueryVariation<Value<Fn>> | NativeQueryVariation<Value<Fn>>
 }
