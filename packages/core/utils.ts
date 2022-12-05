d99fa48900806a59f76522e85cfc3fdd2eb726c0
samuelhulla <oficialny.hulla@gmail.com>
2022-12-05 15:30:21 +0100

    :zap: Feat: getOptions

--- packages/core/utils.ts
+++ packages/core/utils.ts
@@ -1,4 +1,5 @@
-import { ApplyArgs, AsyncFn, ExcludeParams } from './types'
+import type { UseQueryOptions } from '@tanstack/react-query'
+import { ApplyArgs, AsyncFn, Defined, ExcludeParams } from './types'
 
 export function entries<O extends Record<string, unknown>>(obj: O) {
   return Object.entries(obj) as [keyof O, O[keyof O]][]
@@ -14,4 +15,22 @@ export function apply<Fn extends AsyncFn, Params extends ApplyArgs<Fn> & unknown
 ) {
   return (...args: ExcludeParams<Params, Parameters<Fn>>) =>
     fn(...aplyArgs, ...args) as ReturnType<Fn>
+}
+
+export function canEnable<T extends readonly unknown[] | undefined>(arr: T | Defined<T>[]): arr is Array<Defined<T>> {
+  // If the query has no dependencies it will always be enabled
+  if (arr === undefined) {
+    return true
+  }
+  // Otherwise only enable it if all dependencies are provided
+  return arr.every((item) => item !== undefined)
+}
+
+export function getOptions<T, D extends readonly unknown[]>(options?: UseQueryOptions<T>, deps?: D) {
+  if (options === undefined) {
+    return { enabled: canEnable(deps) }
+  }
+  return options.enabled !== undefined
+    ? options
+    : { ...options, enabled: canEnable(deps) }
 }
