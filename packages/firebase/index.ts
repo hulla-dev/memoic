50dfab9d9d452d9bb28f6a35adb52d6431b349a3
samuelhulla <oficialny.hulla@gmail.com>
2022-12-05 12:40:31 +0100

    :zap: Feat: Add support for collection / query fetch

--- packages/firebase/index.ts
+++ packages/firebase/index.ts
@@ -1,6 +1,18 @@
 import { useDoc } from './useDoc'
-import type { Plugin } from '@memoic/core'
+import { useQuery } from './useQuery'
+import { getRef, isDocument } from './utils'
+import {QueryAdapter } from './types'
+import { QueryKey } from '@tanstack/react-query'
 
-export const firebase: Plugin = {
-  get: useDoc,
+
+const fireGet = <Fn extends QueryAdapter>({ queryFn, queryKey }: { queryFn: Fn, queryKey: QueryKey }) => {
+  const ref = getRef(queryFn, queryKey)
+  if (isDocument(ref)) {
+    return useDoc({ queryFn, queryKey })
+  }
+  return useQuery({ queryFn, queryKey })
+}
+
+export const firebase = {
+  get: fireGet,
 }
