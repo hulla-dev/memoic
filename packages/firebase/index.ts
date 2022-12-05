b62075e87fa030725cc54209ca1aa39a17050bbf
samuelhulla <oficialny.hulla@gmail.com>
2022-12-05 16:37:26 +0100

    :rewind: Revert: Default query options

--- packages/firebase/index.ts
+++ packages/firebase/index.ts
@@ -3,16 +3,16 @@ import { useQuery } from './useQuery'
 import { getRef, isDocument } from './utils'
 import { QueryAdapter, Return } from './types'
 import type { QueryKey, UseQueryOptions } from '@tanstack/react-query'
-import { Plugin } from '@memoic/core'
+import { Plugin, getOptions } from '@memoic/core'
 
 
 const fireGet = <Fn extends QueryAdapter>({ queryFn, queryKey, options: opt }: { queryFn: Fn, queryKey: QueryKey, options?: UseQueryOptions<Return<Fn, 'doc' | 'query'>> }) => {
   const ref = getRef(queryFn, queryKey)
   if (isDocument(ref)) {
-    const options = opt as UseQueryOptions<Return<Fn, 'doc'>, Error>
+    const options = getOptions(opt, queryKey) as UseQueryOptions<Return<Fn, 'doc'>, Error>
     return useDoc({ queryFn, queryKey, options })
   }
-  const options = opt as UseQueryOptions<Return<Fn, 'query'>, Error>
+  const options = getOptions(opt, queryKey) as UseQueryOptions<Return<Fn, 'query'>, Error>
   return useQuery({ queryFn, queryKey, options })
 }
 
