5ab4533e74c8084f98019fc5b608e2e946cbf4f4
samuelhulla <oficialny.hulla@gmail.com>
2022-12-06 17:32:15 +0100

    :zap: Feat: Conditional fetch

--- packages/firebase/index.ts
+++ packages/firebase/index.ts
@@ -1,21 +1,25 @@
 import { useDoc } from './useDoc'
 import { useMultiQuery } from './useMultiQuery'
 import { getRef, isDocument } from './utils'
-import { GetReturnVariation, Return } from './types'
+import { GetReturnVariation, ObserverType, Return } from './types'
 import type { QueryKey, UseQueryOptions } from '@tanstack/react-query'
 import { Plugin, getOptions } from '@memoic/core'
+import { useConditional } from './useConditional'
 
-
-const fireGet = <Fn extends (...args: any[]) => GetReturnVariation>({ queryFn, queryKey, options: opt }: { queryFn: Fn, queryKey: QueryKey, options?: UseQueryOptions<Return<Fn, 'doc' | 'query'>> }) => {
+const useGet = <Fn extends (...args: unknown[]) => GetReturnVariation, Opt extends ObserverType>({
+  queryFn,
+  queryKey,
+  options: opt,
+}: {
+  queryFn: Fn
+  queryKey: QueryKey
+  options?: UseQueryOptions<Return<Fn, Opt>>
+}) => {
   const ref = getRef(queryFn, queryKey)
-  if (isDocument(ref)) {
-    const options = getOptions(opt, queryKey) as UseQueryOptions<Return<Fn, 'doc'>, Error>
-    return useDoc({ queryFn, queryKey, options })
-  }
-  const options = getOptions(opt, queryKey) as UseQueryOptions<Return<Fn, 'query'>, Error>
-  return useMultiQuery({ queryFn, queryKey, options })
+  const options = getOptions(opt, queryKey) as UseQueryOptions<Return<Fn, Opt>, Error>
+  return useConditional(isDocument(ref), useDoc, useMultiQuery)({ queryFn, queryKey, options })
 }
 
 export const firebase = {
-  get: fireGet,
+  get: useGet,
 } as Plugin
