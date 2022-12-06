300f8d8e72e24fb07d54d4572a880702cd3d3dbb
samuelhulla <oficialny.hulla@gmail.com>
2022-12-06 17:06:13 +0100

    :zap: Feat: Type overrides on query

--- packages/core/queries.ts
+++ packages/core/queries.ts
@@ -1,7 +1,8 @@
 import type { UseQueryOptions } from '@tanstack/react-query'
+import type { AsyncFn } from './types'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function query<Fn extends (...args: any[]) => any, Result = ReturnType<Fn>>(
+export function query<Fn extends AsyncFn = AsyncFn, Result = ReturnType<Fn>>(
   fn: Fn,
   initialParams?: Parameters<Fn>,
   options?: UseQueryOptions,
