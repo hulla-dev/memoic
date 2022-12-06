36c3c47945719719ae1e6637ec6ee7f97b7c7d52
samuelhulla <oficialny.hulla@gmail.com>
2022-12-06 17:31:46 +0100

    :art: Codestyle: prettier

--- apps/next-example/pages/Child.tsx
+++ apps/next-example/pages/Child.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { get } from '../memoic'
 
 export default function Child() {
-  const { data: users, isFetching } = get('users', [])
+  const { data: users } = get('users', [])
   const { data } = get('user', [(users || [])[0]?.id])
 
   return (
@@ -10,7 +10,9 @@ export default function Child() {
       <h1>Users</h1>
       <ul>
         {users?.map((user) => (
-          <li key={user.id}>{user.id}: {user.name}</li>
+          <li key={user.id}>
+            {user.id}: {user.name}
+          </li>
         ))}
       </ul>
       <h1>Fetched user</h1>
